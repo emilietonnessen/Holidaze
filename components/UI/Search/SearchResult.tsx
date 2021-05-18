9ee0ffe682920b9fa066ce022136ccfedf08539a
Emizzari <emilie@tonnessen.com>
2021-05-18 11:17:29 +0200

    bugfix

--- components/UI/Search/SearchResult.tsx
+++ components/UI/Search/SearchResult.tsx
@@ -2,21 +2,28 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Star } from '../Icons';
-import * as interfaces from '../../../constants/interfaces';
+import {SearchResultProps} from '../../../constants/interfaces';
+import { primary } from "../../../constants/colors";
 
 
-const SearchResult: React.FC<interfaces.SearchResultProps> = ({ name, thumbnail, stars, slug }) => {
+const SearchResult: React.FC<SearchResultProps> = ({ name, thumbnail, stars, slug }) => {
 
     // Calculate the amount of stars that shall be displayed
 	const amountOfStars: number = stars;
-    const calculatedStars: JSX.Element[] = [...Array(amountOfStars)].map((e, i) => <Star key={i} />);
+    const calculatedStars: JSX.Element[] = [...Array(amountOfStars)].map((e, i) => <Star key={i} color={primary} />);
 
     return (
         <Link href={`/establishment/${slug}`}>
             <a className="search-result__link">
                 <div className="search-result">
-                    <Image src={thumbnail} alt="button" width="150" height="100"  />
-                        <div className="search-result__box">
+
+                    <Image 
+                        className="search-result__image" 
+                        src={thumbnail.url} alt="button" 
+                        width={thumbnail.width} 
+                        height={thumbnail.height}  />
+
+                    <div className="search-result__box">
                         <h6 className="search-result__hotel-name">{name}</h6>
                         <div className="search-result__stars">
                             {calculatedStars}
