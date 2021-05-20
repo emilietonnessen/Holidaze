d86207ffeb7788c2e5474c720d1d009ff46f7b8b
Emizzari <emilie@tonnessen.com>
2021-05-20 10:50:09 +0200

    added another grey color

--- constants/colors.ts
+++ constants/colors.ts
@@ -5,5 +5,6 @@ export const greyLightTwo: string = "#D5D5D5";
 export const greyLightThree: string = "#CCC";
 
 export const greyDarkOne: string = "#4A4A4A";
+export const greyDarkTwo: string = "#919191";
 
 export const error: string = "#f10000"
