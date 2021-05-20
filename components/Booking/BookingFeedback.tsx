3a3f478388e4b91c20b3c5a8674ccef41dbd5c2c
Emizzari <emilie@tonnessen.com>
2021-05-20 10:51:33 +0200

    classname bug

--- components/Booking/BookingFeedback.tsx
+++ components/Booking/BookingFeedback.tsx
@@ -1,12 +1,12 @@
 import Modal from '../UI/Modal';
 
 export const BookingFeedbackSuccess = () => (
-    <Modal link="#main" id="feedback-success" classes="modal-feedback">
-        <div className="feedback">
-            <h3 className="feedback__title">
+    <Modal link="#main" id="feedback-success" classes="booking-feedback">
+        <div className="modal-feedback">
+            <h3 className="modal-feedback__title">
                 Thank you!
             </h3>
-            <p className="feedback__text">
+            <p className="modal-feedback__text">
                 Your booking enquiry has been successfully sent. We will contact you very soon!
             </p>
         </div>
