3c56534536b6fa7d16258ba63de54f6c968648a9
Emizzari <emilie@tonnessen.com>
2021-05-20 10:51:39 +0200

    removing unused code

--- components/AdminDashboard/BookingEnquiries/index.tsx
+++ components/AdminDashboard/BookingEnquiries/index.tsx
@@ -11,7 +11,6 @@ import { ENQUIRY_URL } from "../../../constants/api";
 const BookingNotifications: React.FC = () => {
     const [enquiries, setEnquiries] = useState<Booking[]>([]);
     const http: AxiosInstance = useAxios();
-    let url;
 
     useEffect(() => {
         async function fetchData() {
@@ -25,35 +24,6 @@ const BookingNotifications: React.FC = () => {
     }, []);
 
 
-/*     const handler = async () => {
-        url = `${ENQUIRY_URL}/${enquiries.id}`;
-
-        //console.log("[data]", data);
-        //console.log(establishment, read);
-        console.log("[url]", url);
-
-        //data.read = true;
-
-        //const formData = new FormData()
-        //formData.append("read", true);
-
-        //console.log(data.data);
-
-        try {
-            const response = await http.get(url);
-            const data = response;
-
-            console.log("[response]", data);
-            //setNewMessage(true);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            "finally"
-        }   
-    } */
-
-
-
     // Console logs
     //console.log("[Enquiries]", enquiries);
 
@@ -61,7 +31,6 @@ const BookingNotifications: React.FC = () => {
             <BookingCard
                 key={enq.id}
                 id={enq.id}
-                
                 read={enq.read}
                 establishment={enq.establishment}
                 firstName={enq.firstName}
